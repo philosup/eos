d9f02b51bb956c2d3bcbf588c3c473f12b24ba6b
philosup <philosup@gmail.com>
2018-07-05 15:09:49 +0900

    add gacha

--- contracts/sevenchain/sevenchain.cpp
+++ contracts/sevenchain/sevenchain.cpp
@@ -49,14 +49,6 @@ class sevenchain : public eosio::contract
       vector<uint64_t> rng;
 
 	   uint64_t primary_key()const { return id; }
-	   void print() const{
-		   eosio::print("id:",id, "\n");
-         for(const auto &player : participants)
-            player.print();
-         for(auto n : rng)
-            eosio::print(n, ",");
-         eosio::print("\n");
-	   }
       
       EOSLIB_SERIALIZE( poker, (id)(participants)(rng) )
    };
@@ -69,18 +61,36 @@ class sevenchain : public eosio::contract
       vector<uint64_t> rng;
 
 	   uint64_t primary_key()const { return id; }
-	   void print() const{
-		   eosio::print("id:",id, "\n");
-         for(const auto &player : participants)
-            player.print();
-         for(auto n : rng)
-            eosio::print(n, ",");
-         eosio::print("\n");
-	   }
       
       EOSLIB_SERIALIZE( slot, (id)(participants)(rng) )
    };
+   
+   //@abi table 
+   struct item {
+	   uint64_t id;
 
+      vector<struct seven_account> participants;
+      vector<uint64_t> rng;
+
+	   uint64_t primary_key()const { return id; }
+      
+      EOSLIB_SERIALIZE( item, (id)(participants)(rng) )
+   };
+
+   struct iteminfo{
+      string name;
+      uint64_t count;
+   };
+   //@abi table itemcount
+   struct itemcount{
+	   account_name id;
+
+      vector<struct iteminfo> items;
+
+	   uint64_t primary_key()const { return id; }
+ 
+      EOSLIB_SERIALIZE( itemcount, (id)(items))
+   };
 
    typedef eosio::multi_index< N(tableinfo), tableinfo> tableinfos;
    tableinfos tables;
@@ -91,6 +101,14 @@ class sevenchain : public eosio::contract
    typedef eosio::multi_index< N(slot), slot> slot_index;
    slot_index slots;
 
+   typedef eosio::multi_index< N(item), item> item_index;
+   item_index items;
+
+   typedef eosio::multi_index< N(itemcount), itemcount
+     // ,indexed_by<N(byname), const_mem_fun<iteminfo, uint64_t, &iteminfo::primary_key>
+      > itemcount_index;
+   itemcount_index itemcounts;
+
    public:
    using contract::contract;
 
@@ -99,6 +117,8 @@ class sevenchain : public eosio::contract
    ,tables(_self, _self)
    ,pokers(_self, _self)
    ,slots(_self, _self)
+   ,items(_self, _self)
+   ,itemcounts(_self, _self)
    {}
 
 
@@ -160,6 +180,67 @@ class sevenchain : public eosio::contract
        
    }
 
+   //@abi action
+   void startitem(uint64_t id, const vector<struct seven_account> &participants, const vector<uint64_t> &rng)
+   {
+      //todo: participants count is 3
+      //todo: seven_account's hash is 64 bytes
+      //todo: rng count, seven_account random count is 9
+       auto item_itr = items.emplace(_self, [&](auto& r){
+           r.id = id;
+           r.participants.assign(participants.begin(), participants.end());
+           r.rng.assign(rng.begin(), rng.end());
+      });
+       
+       auto info = tables.find(1);
+       if(info == tables.end()) {
+          tables.emplace(_self, [&](auto& r){
+             r.id = 1;
+             r.tablename = "item";
+             r.last_id = item_itr->id;
+             r.count = 1;
+          });
+       }else{
+          tables.modify(info, _self,  [&](auto& r){
+             r.last_id = item_itr->id;
+             r.count++;
+          });
+       }
+       
+   }
+
+   //@abi action
+   void saveiteminfo(const account_name id, const vector<struct iteminfo> &iteminfos)
+   {
+      print(id,", ", N(id));
+      auto it = itemcounts.find(id);
+      if(it == itemcounts.end())
+      {
+         itemcounts.emplace(_self, [&](auto& r){
+            r.id = id;
+            r.items.assign(iteminfos.begin(), iteminfos.end());
+         });
+      }
+      else{
+         itemcounts.modify(it, _self, [&](auto& r){
+            for(const auto& info : iteminfos)
+            {
+               bool exists = false;
+               for(auto& dbinfo : r.items){
+                  if(dbinfo.name == info.name)
+                  {
+                     dbinfo.count += info.count;
+                     exists = true;
+                     break;
+                  }
+               }
+               if(exists == false)
+                  r.items.push_back(info);
+            }
+         });
+      }
+   }
+
 };
 
-EOSIO_ABI(sevenchain, (startpoker)(startslot))
+EOSIO_ABI(sevenchain, (startpoker)(startslot)(startitem)(saveiteminfo))
